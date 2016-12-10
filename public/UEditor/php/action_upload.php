33daaabb46fbf59affab9037aa4ee80f400f83e1
hi@xiaoguan.net <hi@xiaoguan.net>
2016-12-11 00:13:41 +0800

    *UEditor编辑器集成UPYun-完成单图上传

--- public/UEditor/php/action_upload.php
+++ public/UEditor/php/action_upload.php
@@ -11,12 +11,49 @@ include "Uploader.class.php";
 $base64 = "upload";
 switch (htmlspecialchars($_GET['action'])) {
     case 'uploadimage':
-        $config = array(
-            "pathFormat" => $CONFIG['imagePathFormat'],
-            "maxSize" => $CONFIG['imageMaxSize'],
-            "allowFiles" => $CONFIG['imageAllowFiles']
-        );
-        $fieldName = $CONFIG['imageFieldName'];
+        include "upyun.class.php";
+        //文章专用upyun空间
+        $bucket='qdzufang';//upyun空间名
+        $upyun_user='qdzufang';//操作员
+        $upyun_psw='521my1!@#';//密码
+        $upyun_path='/weixin/qdzufang/'.date("Ymd").'/';//上传路径
+        if($bucket&&$upyun_user&&$upyun_psw){
+            $oldname=$_FILES[ "upfile" ]["name"];
+            $filesize=$_FILES['upfile']['size'] ;
+            $filepath = $_FILES[ "upfile" ][ 'tmp_name' ];
+            $filetype = pathinfo($oldname, PATHINFO_EXTENSION);
+            $newname='UE'.date("YmdHis").'.'.$filetype;
+            $upyun = new UpYun($bucket, $upyun_user, $upyun_psw);
+            //echo "<script>console.log('".$newname."')</script>";
+            try {
+                $fh = fopen($_FILES[ "upfile" ][ 'tmp_name' ], 'rb');
+                $rsp = $upyun->writeFile($upyun_path.$newname, $fh, True);   // 上传图片，自动创建目录
+                fclose($fh);
+                $file = 'http://'.$bucket.'.b0.upaiyun.com'.$upyun_path.$newname;
+                //echo "<script>console.log('".$file."')</script>";
+                $info=array(
+                    "state" => 'SUCCESS',
+                    "url" => $file,
+                    "title" => $newname,
+                    "original" => str_replace(".".$filetype,"",$oldname),
+                    "type" => $filetype,
+                    "size" => $filesize
+                );
+                /* 返回数据 */
+                return json_encode($info);
+            }
+            catch(Exception $e) {
+                /* 返回数据 */
+                return json_encode(array("state" => 'FALSE'));
+            }
+        }else {
+            $config = array(
+                "pathFormat" => $CONFIG['imagePathFormat'],
+                "maxSize" => $CONFIG['imageMaxSize'],
+                "allowFiles" => $CONFIG['imageAllowFiles']
+            );
+            $fieldName = $CONFIG['imageFieldName'];
+        }
         break;
     case 'uploadscrawl':
         $config = array(
--- public/UEditor/php/controller.php
+++ public/UEditor/php/controller.php
@@ -1,5 +1,4 @@
 <?php
-
 //header('Access-Control-Allow-Origin: http://www.baidu.com'); //设置http://www.baidu.com允许跨域访问
 //header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With'); //设置允许的跨域header
 date_default_timezone_set("Asia/chongqing");
@@ -13,30 +12,48 @@ switch ($action) {
     case 'config':
         $result =  json_encode($CONFIG);
         break;
+
+    /* 上传图片 */
     case 'uploadimage':
-        use Illuminate\Http\Request;
-        use App\Http\Requests;
-        $result = json_encode(array(
-            'file'=> $req->upfile
-        ));
+    /* 上传涂鸦 */
+    case 'uploadscrawl':
+    /* 上传视频 */
+    case 'uploadvideo':
+    /* 上传文件 */
+    case 'uploadfile':
+        $result = include("action_upload.php");
         break;
-   default:
+
+    /* 列出图片 */
+    case 'listimage':
+        $result = include("action_list.php");
+        break;
+    /* 列出文件 */
+    case 'listfile':
+        $result = include("action_list.php");
+        break;
+
+    /* 抓取远程文件 */
+    case 'catchimage':
+        $result = include("action_crawler.php");
+        break;
+
+    default:
         $result = json_encode(array(
             'state'=> '请求地址出错'
         ));
         break;
 }
 
-echo $result;
 /* 输出结果 */
-//if (isset($_GET["callback"])) {
-//    if (preg_match("/^[\w_]+$/", $_GET["callback"])) {
-//        echo htmlspecialchars($_GET["callback"]) . '(' . $result . ')';
-//    } else {
-//        echo json_encode(array(
-//            'state'=> 'callback参数不合法'
-//        ));
-//    }
-//} else {
-//    echo $result;
-//}
+if (isset($_GET["callback"])) {
+    if (preg_match("/^[\w_]+$/", $_GET["callback"])) {
+        echo htmlspecialchars($_GET["callback"]) . '(' . $result . ')';
+    } else {
+        echo json_encode(array(
+            'state'=> 'callback参数不合法'
+        ));
+    }
+} else {
+    echo $result;
+}
